e427e854ccce18ec718aebf65a5015045271f2b8
Marisa Hanna <m.e.hanna1983@hotmail.com>
2021-07-19 22:02:32 -0700

    models folder

---
+++ models/Books.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    
+  title: {
+    type: String,
+    required: true,
+  },
+
+  authors: {
+    type: Array,
+    required: false,
+  },
+
+  description: {
+    type: String,
+    required: false,
+  },
+
+  link: {
+    type: String,
+    required: false,
+  },
+
+  image: {
+    type: String,
+    required: false,
+  },
+
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
