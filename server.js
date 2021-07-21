d058c2a4396962b2e505c1b62108b5e2f6304ebd
Marisa Hanna <m.e.hanna1983@hotmail.com>
2021-07-20 22:46:56 -0700

    updating server

--- server.js
+++ server.js
@@ -17,8 +17,13 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
-);
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",{
+  useNewUrlParser: true
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 // Start the API server
 app.listen(PORT, function() {
