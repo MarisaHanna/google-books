fe16567a94ecf7b47ff10d898a6b3067f77591de
Marisa Hanna <m.e.hanna1983@hotmail.com>
2021-07-21 14:05:28 -0700

    ran npm run build

---
+++ client/build/precache-manifest.a37fe7dc0d5fc658976380a533730ff8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd50bdd5ff79233bc78869963449c4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cfdd342a2591135cbea",
+    "url": "/static/css/main.6b8169a8.chunk.css"
+  },
+  {
+    "revision": "5ca2901bb1f59f24587c",
+    "url": "/static/js/2.e1169998.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "/static/js/2.e1169998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cfdd342a2591135cbea",
+    "url": "/static/js/main.392a93f9.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
